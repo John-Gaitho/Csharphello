81a57648586f06492a8ea0bff4e32f71cc1447fd
John Gaitho <jgaitho016@gmail.com>
2025-11-27 22:42:04 +0300

    hello

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace CSharpHello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from C#! Welcome to my first console app.whats up?");
+        }
+    }
+}
